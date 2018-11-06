be7004fe3934d2203c9f04081dfed1bfbce0f250
G Singh <gssrann@gmail.com>
2018-11-06 12:19:57 +0000

    bug fix

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const port = process.env.PORT || 4000;
